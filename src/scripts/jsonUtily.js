331c6113e44448ee21eed5a8109ea1082e9bcc2e
Tristan Forward <tforward@users.noreply.github.com>
2019-03-11 20:19:40 -0400

    work

--- src/scripts/jsonUtily.js
+++ src/scripts/jsonUtily.js
@@ -6,7 +6,20 @@ export function log(data) {
 
 export function configJson(lyrName, data) {
   const lyrGroup = rtnLayerGroup(data);
-  return R.find(R.propEq("id", lyrName))(data[lyrGroup]);
+
+  const lyrObj = R.find(R.propEq("id", lyrName))(data[lyrGroup]);
+  const lyrs = R.prop("layers", lyrObj);
+
+  const layerIdsTitles = R.map(pickIdTitle, lyrs);
+  log(layerIdsTitles);
+
+  return lyrObj;
+}
+function pickIdTitle(lyr) {
+  const id = R.pick(["id"], lyr);
+  const title = R.prop(["title"], lyr.popupInfo);
+  const justTitle = R.head(R.take(1, R.split(":", title)));
+  return R.mergeRight(id, { title: justTitle });
 }
 
 function rtnLayerGroup(data) {
